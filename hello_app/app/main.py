8e5c2ae2cb8a5e29a85b5dbf5f85b96fadbf83c8
Claudio Cordova <claudio_cordova@yahoo.com>
2023-06-17 16:53:16 -0700

    hello v2

--- hello_app/app/main.py
+++ hello_app/app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def msg():
-    return "Hello World! Deployed using Argo CD!"
+    return "Hello World! Deployed using Argo CD! Cool!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
